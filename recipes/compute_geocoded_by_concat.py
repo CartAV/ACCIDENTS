b6c8820417654b33afcd9dbca742a476aa02fee2
chabouisfr <chabouisfr>
2017-01-10 12:05:30 +0100

    Saved recipe: ACCIDENTS.compute_geocoded_by_concat

--- recipes/compute_geocoded_by_concat.py
+++ recipes/compute_geocoded_by_concat.py
@@ -56,7 +56,8 @@ def adresse_submit(df):
         response = requests_session.request(**kwargs)
         if (response.status_code == 200):
             res=pd.read_csv(StringIO.StringIO(response.content.decode('utf-8')),sep=",",quotechar='"')
-            res=pd.merge(df,res,how='left',on=['Num_Acc','adr','city_code'])
+            #res=pd.merge(df,res,how='left',on=['Num_Acc','adr','city_code'])
+            res=pd.merge(df,res,how='left',on=['Num_Acc'])
             #print(res)
             t=maxtries+1
         elif (response.status_code == 400):
