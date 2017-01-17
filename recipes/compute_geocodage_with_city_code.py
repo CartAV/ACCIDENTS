c1fd9c76c7a9675dcdaaf9b2ecd9447c45f5387e
grabenertr <grabenertr>
2017-01-17 18:05:56 +0100

    Saved recipe: ACCIDENTS.compute_geocodage_with_city_code

--- recipes/compute_geocodage_with_city_code.py
+++ recipes/compute_geocodage_with_city_code.py
@@ -38,7 +38,7 @@ class GeoHisto(object):
             self.towns = mydataset.get_dataframe(infer_with_pandas=False)
             self.towns.fillna(value={"successors":""}, inplace=True)
             self.towns.set_index('id', inplace=True)
-            self.towns = self.towns.head(n=1000)
+            self.towns = self.towns.head(n=10000)
         except ImportError:
             self.towns = pd.read_csv('towns.csv',
                                      index_col='id',
