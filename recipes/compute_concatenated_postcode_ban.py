d03a24c15b944854e107a13e822d1d5c62212fa1
grabenertr <grabenertr>
2017-01-18 17:38:18 +0100

    Saved recipe: ACCIDENTS.compute_concatenated_postcode_ban

--- recipes/compute_concatenated_postcode_ban.py
+++ recipes/compute_concatenated_postcode_ban.py
@@ -55,6 +55,7 @@ def adresse_submit(df):
         response = requests_session.request(**kwargs)
         if (response.status_code == 200):
             res=pd.read_csv(StringIO.StringIO(response.content.decode('utf-8')),sep=",",quotechar='"')
+            print res
             res=pd.merge(df,res,how='left',on=['Num_Acc','adr','city_code'])
             #print(res)
             t=maxtries+1
