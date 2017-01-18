0864d9f12112fe466583793e3a029636dadfb13e
grabenertr <grabenertr>
2017-01-18 18:14:13 +0100

    Saved recipe: ACCIDENTS.compute_concatenated_postcode_ban

--- recipes/compute_concatenated_postcode_ban.py
+++ recipes/compute_concatenated_postcode_ban.py
@@ -42,7 +42,7 @@ def adresse_submit(df):
             'data': {                     
                         'columns':'adr', 
                         'citycode':'city_code',
-                        'with-bom': 'false',
+                        'with-bom': 'false'
             },
             'method': 'post',
             'files': OrderedDict([
