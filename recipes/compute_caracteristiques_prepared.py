04266e17c3a090503c52418ea257c9c19ab2cb25
chabouisfr <chabouisfr>
2017-01-23 17:43:14 +0100

    Saved recipe: ACCIDENTS.compute_caracteristiques_prepared

--- recipes/compute_caracteristiques_prepared.py
+++ recipes/compute_caracteristiques_prepared.py
@@ -9,10 +9,9 @@ import pandas as pd
 
 def fillna(dataset):
     return dataset.fillna(axis=1, value={
-        'col': '0',
-        'com': '000',
-        'atm': '1',
-        })
+        'col': '0'}).fillna(axis=1, value={
+        'com': '000'}).fillna(axis=1, value={
+        'atm': '1'})
 
 
 def invalid_coordinates(lon, lat):
