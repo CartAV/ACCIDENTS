f1874f6d4623435f2de5057ef49760a4adb18160
grabenertr <grabenertr>
2017-03-02 17:16:08 +0100

    Saved recipe: ACCIDENTS.compute_accidents_pg_prep2

--- recipes/compute_accidents_pg_prep2.sql
+++ recipes/compute_accidents_pg_prep2.sql
@@ -1,5 +1,5 @@
 SELECT *
-FROM accidents_pg_prep, 
+FROM accidents_pg_prep 
     LEFT JOIN LATERAL (SELECT "ID_RTE500", st_distance(geom_acc, the_geom) as distance_route_accident
      FROM "public"."ign_troncon_route_postgis"
      where st_dwithin(geom_acc, "the_geom", 50)
