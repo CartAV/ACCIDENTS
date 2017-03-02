82a52cda8ae92da16bc3877fb181610bf121478d
grabenertr <grabenertr>
2017-03-02 16:49:23 +0100

    Saved recipe: ACCIDENTS.compute_accidents_pg_prep2

--- recipes/compute_accidents_pg_prep2.sql
+++ recipes/compute_accidents_pg_prep2.sql
@@ -2,6 +2,6 @@ SELECT *
 FROM accidents_pg_prep, 
     LATERAL (SELECT "ID_RTE500", st_distance(geom_acc, the_geom) as distance_route_accident
      FROM "public"."ign_troncon_route_postgis"
-     where geom_acc && "the_geom"
+     where st_dwithin(geom_acc, "the_geom") < 100
      ORDER BY distance_route_accident
     LIMIT 1) as nearest_route
