54620b10570702558089e02e5c310af0a6ab45d1
grabenertr <grabenertr>
2017-03-02 17:29:27 +0100

    Saved recipe: ACCIDENTS.compute_accidents_pg_prep2

--- recipes/compute_accidents_pg_prep2.sql
+++ recipes/compute_accidents_pg_prep2.sql
@@ -1,8 +1,12 @@
 SELECT *
 FROM accidents_pg_prep 
-    LEFT JOIN LATERAL (SELECT "ID_RTE500", st_distance(geom_acc, the_geom) as distance_route_accident
-     FROM "public"."ign_troncon_route_postgis"
-     where st_dwithin(geom_acc, "the_geom", 100)
-     ORDER BY distance_route_accident
-    LIMIT 1) as nearest_route
+    LEFT JOIN LATERAL (
+        SELECT "ID_RTE500",
+               st_distance(geom_acc, the_geom) as distance_route_accident,
+               st_ClosestPoint(the_geom, geom_acc) as projected_point
+        FROM "public"."ign_troncon_route_postgis"
+        WHERE st_dwithin(geom_acc, "the_geom", 100)
+        ORDER BY distance_route_accident
+        LIMIT 1
+    ) as nearest_route
     ON true
