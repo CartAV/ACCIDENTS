42854cdd619d4ca392b1a1fe7131787ce4d34178
grabenertr <grabenertr>
2017-03-03 13:49:17 +0100

    Saved recipe: ACCIDENTS.compute_accidents_pg_prep2

--- recipes/compute_accidents_pg_prep2.sql
+++ recipes/compute_accidents_pg_prep2.sql
@@ -3,7 +3,7 @@ FROM accidents_pg_prep
     LEFT JOIN LATERAL (
         SELECT "ID_RTE500",
                st_distance(geom_acc, the_geom) as distance_route_accident,
-               st_ClosestPoint(geom_acc::geometry, the_geom::geometry) as projected_point
+               st_ClosestPoint(the_geom::geometry, geom_acc::geometry) as projected_point
         FROM "public"."ign_troncon_route_postgis"
         WHERE st_dwithin(geom_acc, "the_geom", 100)
         ORDER BY distance_route_accident
