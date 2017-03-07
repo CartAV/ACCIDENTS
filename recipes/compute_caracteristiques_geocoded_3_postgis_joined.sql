bdbc1c76a1a63ee97fc82d0f61b0112993037400
grabenertr <grabenertr>
2017-03-07 11:27:10 +0100

    Saved recipe: ACCIDENTS.compute_caracteristiques_geocoded_3_postgis_joined

--- recipes/compute_caracteristiques_geocoded_3_postgis_joined.sql
+++ recipes/compute_caracteristiques_geocoded_3_postgis_joined.sql
@@ -41,6 +41,10 @@ SELECT
     "caracteristiques_geocoded_3_postgis"."ban2_score" AS "ban2_score",
     "caracteristiques_geocoded_3_postgis"."ban2_type" AS "ban2_type",
     st_distance("IGN_COMMUNE_FRANCE".the_geom, st_point(long, lat)) as distance_commune_baac,
+    st_distance("IGN_COMMUNE_FRANCE".the_geom, st_point(ban_longitude, ban_latitude)) as distance_commune_ban,
+    st_distance("IGN_COMMUNE_FRANCE".the_geom, st_point(ban2_longitude, ban2_latitude)) as distance_commune_ban2,
+    st_distance("IGN_COMMUNE_FRANCE".the_geom, st_point(bano_longitude, bano_latitude)) as distance_commune_bano,
+
     "IGN_COMMUNE_FRANCE"."CODE_COM" AS "CODE_COM"
   FROM "caracteristiques_geocoded_3_postgis"
   LEFT JOIN "ign_commune_france" "IGN_COMMUNE_FRANCE"
